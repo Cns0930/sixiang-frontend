2d85e75f9068ef87a972745f8052f01213c96daf
liwenchao <liwenchao@seassoon.com>
2020-05-31 15:12:54 +0800

    实现 中文 index

--- src/utils/handlebarsHelper.js
+++ src/utils/handlebarsHelper.js
@@ -1,12 +1,32 @@
 import Handlebars from "handlebars"
+import indexCn from "./indexCn.json"
+Handlebars.registerHelper('equal', function (arg1, arg2, options) {
 
-Handlebars.registerHelper('equal', function(arg1, arg2, options){
-			
-    if(arg1 == arg2) {
-      return options.fn(this)
-    }else{
-      return options.inverse(this)
+    if (arg1 == arg2) {
+        return options.fn(this)
+    } else {
+        return options.inverse(this)
     }
 });
+
+
+Handlebars.registerHelper("setVar", function (varName, varValue, options) {
+    console.log(options.data.root)
+    options.data.root[varName] = varValue;
+
+});
+Handlebars.registerHelper("inc", function (name, options) {
+   
+    options.data.root[name] = options.data.root[name] + 1;
+    return options.data.root[name];
+});
+
+Handlebars.registerHelper("incInCn", function (index, options) {
+
+    
+
+    return indexCn[index.toString()];
+});
+
 
 export default Handlebars
