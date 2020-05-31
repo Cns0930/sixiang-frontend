c16f849e4868fe555951b87386b69d2caa2f1865
liwenchao <liwenchao@seassoon.com>
2020-05-31 16:38:02 +0800

    条件 语法

--- src/utils/handlebarsHelper.js
+++ src/utils/handlebarsHelper.js
@@ -11,24 +11,24 @@ Handlebars.registerHelper('equal', function (arg1, arg2, options) {
 
 
 Handlebars.registerHelper("setVar", function (varName, varValue, options) {
-  
+
     options.data.root[varName] = varValue;
 
 });
 Handlebars.registerHelper("inc", function (name, options) {
-   
+
     options.data.root[name] = options.data.root[name] + 1;
     return options.data.root[name];
 });
 
 Handlebars.registerHelper("indexInCn", function (index, options) {
 
-    
+
 
     return indexCn[index.toString()];
 });
 
-Handlebars.registerHelper("includes", function (arr,value, options) {
+Handlebars.registerHelper("includes", function (arr, value, options) {
 
 
     if (arr.includes(value)) {
@@ -36,9 +36,64 @@ Handlebars.registerHelper("includes", function (arr,value, options) {
     } else {
         return options.inverse(this)
     }
-    
+
 });
 
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
+
+Handlebars.registerHelper('cond', function (v1, operator, v2, options) {
+
+    switch (operator) {
+        case '==':
+            return (v1 == v2);
+        case '===':
+            return (v1 === v2);
+        case '!=':
+            return (v1 != v2);
+        case '!==':
+            return (v1 !== v2);
+        case '<':
+            return (v1 < v2);
+        case '<=':
+            return (v1 <= v2);
+        case '>':
+            return (v1 > v2);
+        case '>=':
+            return (v1 >= v2);
+        case '&&':
+            return (v1 && v2);
+        case '||':
+            return (v1 || v2);
+        default:
+            return false;
+    }
+});
 
 
 export default Handlebars
