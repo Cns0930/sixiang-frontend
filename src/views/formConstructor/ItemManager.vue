5a0f17cdb0344bd2ea3bd25e704e8eba4d75ddeb
chime <yepeixuan@seassoon.com>
2020-07-25 14:50:59 +0800

    创建日期筛选事项

--- src/views/formConstructor/ItemManager.vue
+++ src/views/formConstructor/ItemManager.vue
@@ -3,7 +3,19 @@
         <div class="op-bar">
             <el-button @click="handleEditItem({});">创建新事项</el-button>
         </div>
+
         <div class="main">
+            <div class="time-filter" style="width: 100%;padding:10px 60px">
+                <span class="demonstration">筛选事项创建时间:</span>
+                <el-date-picker
+                    v-model="dateRange"
+                    type="daterange"
+                    range-separator="-"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期"
+                ></el-date-picker>
+                <el-button @click="filterDate();" size="small">确认</el-button>
+            </div>
             <!-- 事项表格 -->
             <div class="item-table" style="width: 100%;padding:10px 60px">
                 <el-table :data="itemlist" border style="width: 100%">
@@ -18,7 +30,12 @@
                         </template>
                     </el-table-column>
                     <el-table-column prop="department" label="委办局" width="120"></el-table-column>
-                    <el-table-column prop="createTime" :formatter="dateFormat" label="创建时间" width="120"></el-table-column>
+                    <el-table-column
+                        prop="createTime"
+                        :formatter="dateFormat"
+                        label="创建时间"
+                        width="120"
+                    ></el-table-column>
                     <el-table-column label="操作" width="120">
                         <template slot-scope="scope">
                             <el-button
@@ -45,35 +62,25 @@
         >
             <div class="attribute-content">
                 sid
-                <el-input v-model="tempItem.sid"></el-input>
-                审批事项名称
-                <el-input v-model="tempItem.name"></el-input>
-                审批事项英文代号(拼音)
-                <el-input v-model="tempItem.itemCode"></el-input>
-                审批事项大类
-                <el-input v-model="tempItem.category"></el-input>
-                属性
-                <el-input v-model="tempItem.type"></el-input>
-                所属委办局
+                <el-input v-model="tempItem.sid"></el-input>审批事项名称
+                <el-input v-model="tempItem.name"></el-input>审批事项英文代号(拼音)
+                <el-input v-model="tempItem.itemCode"></el-input>审批事项大类
+                <el-input v-model="tempItem.category"></el-input>属性
+                <el-input v-model="tempItem.type"></el-input>所属委办局
                 <div>
-                <el-select v-model="tempItem.department">
-                    <el-option
-                        v-for="(v,i) in departmentOptions"
-                        :key="i"
-                        :label="v.department"
-                        :value="v.department"
-                    ></el-option>
-                </el-select>
-                </div>
-                事权性质
-                <el-input v-model="tempItem.authority"></el-input>
-                序号
-                <el-input v-model="tempItem.seq"></el-input>
-                备注
-                <el-input v-model="tempItem.note"></el-input>
-                激活状态
-                <el-input v-model="tempItem.status"></el-input>
-                所在区域
+                    <el-select v-model="tempItem.department">
+                        <el-option
+                            v-for="(v,i) in departmentOptions"
+                            :key="i"
+                            :label="v.department"
+                            :value="v.department"
+                        ></el-option>
+                    </el-select>
+                </div>事权性质
+                <el-input v-model="tempItem.authority"></el-input>序号
+                <el-input v-model="tempItem.seq"></el-input>备注
+                <el-input v-model="tempItem.note"></el-input>激活状态
+                <el-input v-model="tempItem.status"></el-input>所在区域
                 <el-input v-model="tempItem.area"></el-input>
             </div>
             <span slot="footer" class="dialog-footer">
@@ -89,10 +96,12 @@ import {
     listAll,
     saveItem,
     listDepartment,
-    deleteItem
+    deleteItem,
+    filterDateItem,
 } from "@/api/item/index";
 import axios from "../../api/config";
 import dayjs from "dayjs";
+import { parse } from "handlebars";
 export default {
     name: "ItemManeger",
     data() {
@@ -101,6 +110,7 @@ export default {
             tempItem: {},
             dialogVisible: false,
             departmentOptions: [],
+            dateRange: "",
         };
     },
     mounted() {
@@ -113,37 +123,80 @@ export default {
                 this.$message({ type: "warning", message: "获取初始数据失败" });
             }
             this.itemlist = result.data;
+
+            this.itemlist.sort((a, b) => {
+                return dayjs(b.createTime) - dayjs(a.createTime);
+            });
+            console.log(this.itemlist);
+
             let departments = await listDepartment();
             this.departmentOptions = departments.data;
         },
         async saveItem() {
             let result = await saveItem(this.tempItem);
             if (!result.success) return;
-            this.$message({ type: "success", message: "保存成功" });
-            this.load();
+            this.$message({ type: "success", message: "保存成功 请重新筛选时间后查看数据" });
+            this.filterDate();
         },
         async handleDeleteItem(v) {
-            let message = '确定要删除'+v.name+'吗';
+            let message = "确定要删除" + v.name + "吗";
             if (confirm(message) == true) {
                 let result = await deleteItem({ itemId: v.id });
                 if (!result.success) return;
-                this.$message({ type: "success", message    : "删除成功" });
-                this.load();
+                this.$message({ type: "success", message: "删除成功" });
+                this.filterDate();
             }
         },
         handleClickItem(item) {
             this.$store.commit("changeItem", item);
-            this.$router.push({path:'/formconstructor', query:{'itemId':item.id}});
+            this.$router.push({
+                path: "/formconstructor",
+                query: { itemId: item.id },
+            });
         },
         handleEditItem(item) {
             this.tempItem = item;
             this.dialogVisible = true;
         },
-        dateFormat(row, column, cellValue, index){
-          const daterc = row[column.property]
-          return dayjs(daterc).format("YYYY-MM-DD");
-        }
-    }
+        dateFormat(row, column, cellValue, index) {
+            const daterc = row[column.property];
+            return dayjs(daterc).format("YYYY-MM-DD");
+        },
+        async filterDate() {
+            console.log(this.dateRange);
+            if (this.dateRange) {
+                let params = {
+                    startDate: this.dateRange[0],
+                    endDate: this.dateRange[1],
+                };
+                let result = await filterDateItem(params);
+                if (!result.success) {
+                    this.$message({
+                        type: "warning",
+                        message: "获取数据失败",
+                        
+                    });
+                    this.itemlist = []
+                    return
+                }
+                this.itemlist = result.data;
+            } else {
+                let result = await listAll();
+                if (!result.success) {
+                    this.$message({
+                        type: "warning",
+                        message: "获取数据失败",
+                    });
+                    this.itemlist = []
+                    return
+                }
+                this.itemlist = result.data;
+            }
+            this.itemlist.sort((a, b) => {
+                return dayjs(b.createTime) - dayjs(a.createTime);
+            });
+        },
+    },
 };
 </script>
 
