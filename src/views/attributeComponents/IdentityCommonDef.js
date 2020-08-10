c920209538c037aa3c959e4b77e78eaf1a0aa44a
liwenchao <liwenchao@seassoon.com>
2020-08-10 14:39:18 +0800

    shenfz 属性

--- src/views/attributeComponents/IdentityCommonDef.js
+++ src/views/attributeComponents/IdentityCommonDef.js
@@ -8,6 +8,7 @@ class IdentityCommonDef extends CommonComponents{
         this.span = rendererBuilder("text",24)
         this.value = rendererBuilder("text",{idPositive:"",idNagetive:""})
         this.labelWidth =  new Renderer('ElInput', '0')
+        this.valueDescription = new Renderer('textSpan', '{name:身份证名称,code:身份证号,address:身份证地址,idNagetive:身份证国徽面,idPositive:身份证人像面}',null,"value结构")
     }
     getAttributes(){
         return {
