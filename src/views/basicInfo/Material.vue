4ea01def71d32734afce04f7563884a6132288ec
wuyueru <wuyueru@seassoon.com>
2020-09-16 11:15:43 +0800

    ...

--- src/views/basicInfo/Material.vue
+++ src/views/basicInfo/Material.vue
@@ -272,21 +272,15 @@ export default {
                 materialId: v.materialId
             };
             let id = v.materialId;
-   await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          });
-        }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '已取消删除'
-          });          
-        });
+            try{
+                await this.$confirm('此操作将永久删除该, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+                })
+            }catch(e){
+                return;
+            }
     
             let result = await delMaterial(param);
             if (!result.success) return;
