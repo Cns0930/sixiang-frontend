7ced577d1c10c5cac30e9c6fda5aa17b4eab65b5
gongjun <gongjun@seassoon.com>
2020-09-17 15:18:22 +0800

    添加情形选择默认勾选和禁用属性

--- src/views/attributeComponents/QingxingCheckboxDef.js
+++ src/views/attributeComponents/QingxingCheckboxDef.js
@@ -11,6 +11,7 @@ let ListInput = {
         let scopedSlotsCode ={default:(scope)=><el-input vModel={scope.row.code} style="width:100%"></el-input>}
         let scopedSlotsDisabled ={default:(scope)=><el-checkbox vModel={scope.row.disabled} ></el-checkbox>}
         let scopedSlotsHidden ={default:(scope)=><el-checkbox vModel={scope.row.hidden} ></el-checkbox>}
+        let scopedSlotsChosen ={default:(scope)=><el-checkbox vModel={scope.row.chosen} ></el-checkbox>}
         let scopedSlotOp ={default:(scope)=><div>
             <el-button onclick={() => { this.value.splice(scope.$index + 1, 0, {label:"",value:"",code:"",disabled:false}) }}>添加</el-button>
             <el-button onClick={() => { this.value.splice(scope.$index, 1) }}>删除</el-button>
@@ -22,6 +23,7 @@ let ListInput = {
                 <el-table-column prop="code" label="情形编号" scopedSlots={scopedSlotsCode}> </el-table-column>
                 <el-table-column prop="disabled" label="是否禁用" scopedSlots={scopedSlotsDisabled}> </el-table-column>
                 <el-table-column prop="hidden" label="是否默认隐藏" scopedSlots={scopedSlotsHidden}> </el-table-column>
+                <el-table-column prop="chosen" label="是否默认选中" scopedSlots={scopedSlotsChosen}> </el-table-column>
                 <el-table-column  label="操作" scopedSlots={scopedSlotOp}> </el-table-column>
             </ElTable>
     }
