cf533bd1d3e42a991d5dda3c25df71a666f1f835
liwenchao <liwenchao@seassoon.com>
2021-01-07 15:47:38 +0800

    完善 qrcode组件

--- src/views/layoutComponents/QRCodeUpload/QRCodeUpload.vue
+++ src/views/layoutComponents/QRCodeUpload/QRCodeUpload.vue
@@ -1,38 +1,79 @@
 <template>
-    <div>
-        <div v-if="!status" class="container-init" :style="style">
-            <div>
-                <img src="./img/boader.png">pic1</img>
-                <img>pic2</img>
+    <div :style="style" class="container">
+        <div v-if="!value" class="container-init">
+            <div class="left">
+
+                <div class="left-top"></div>
+                <div class="right-top"></div>
+                <div class="left-bottom"></div>
+                <div class="right-bottom"></div>
+                <div class="center">
+                    <img src="./img/imgPlaceholder.png" />
+                    <p class="content">{{content}}</p>
+                </div>
             </div>
-            <div>
-                
+            <div class="right" @click="handleOpenQR">
+                <img src="./img/urcode.png" />
+                 <p class="content">二维码上传</p>
             </div>
 
         </div>
-        <div class="container-img" v-else>
-
+        <div class="container-img"  :style="{borderRadius:borderRadius+'px'}" v-else>
+            <el-image 
+                style="width: 100%; "
+               
+                :src="value" 
+                :preview-src-list="srcList">
+            </el-image>
+            <div class="right-delete" @click="handleDelete">
+                <i class="el-icon-delete"></i>
+                <p>删除</p>
+            </div>
+            <!-- <img :src="value" /> -->
         </div>
-        <el-dialog title="">
-
+        <el-dialog title="扫码上传图片" :visible.sync="openQR">
+            <div class="dialog-body">
+                <vue-qr :text="url" :callback="qrCallback" qid="testid"></vue-qr>
+            </div>
+            
         </el-dialog>
     </div>
 
 </template>
 
 <script>
-
+import VueQr from 'vue-qr'
+import axios from 'axios'
+import dayjs from "dayjs"
 export default {
     name: "QRCodeUpload",
-    props: ["value", "content", "height", "borderRadius"],
+    props: ["value", "content", "height", "borderRadius","uploadUrlDev","getImgUrlDev","uploadUrlProd","getImgUrlProd","milliseconds"],
     data() {
         return {
-
+            openQR:false,
+            url:"",
+            uid:"",
+            timer:null,
+            updateTime:null,
         }
     },
+    beforeDestroy(){
+        if(this.timer){
+            clearInterval(this.timer)
+        }
+        
+    },
+    components:{VueQr},
+    async created(){
+        let uploadUrl = process.env.NODE_ENV === 'development'?this.uploadUrlDev:this.uploadUrlProd
+        let result = await axios.get(uploadUrl).then(res => res.data)
+        if(result.message!="SUCCESS") return;
+        this.url = result.data.url;
+        this.uid= result.data.uid
+    },
     computed: {
-        status() {
-            return !!this.value
+        srcList(){
+            return [this.value]
         },
         style() {
             return {
@@ -40,17 +81,158 @@ export default {
                 borderRadius: this.borderRadius + "px"
             }
         }
+    },
+    methods:{
+        handleOpenQR(){
+            this.openQR=true;
+            //  轮询 查询图片
+            console.log("询start",this.milliseconds)
+            this.timer = setInterval(this.queryImg,this.milliseconds)
+        },
+        async queryImg(){
+            console.log("询start",this.uid)
+            if(!this.uid) return;
+            let getImgUrl = process.env.NODE_ENV === 'development'?this.getImgUrlDev:this.getImgUrlProd
+            let result = await axios(getImgUrl,{params:{uid:this.uid}}).then(res => res.data)
+            if(result.message!="SUCCESS") return;
+            if(result.data.length<1) return;
+            console.log(result.data)
+            let {ossUrl,updateTime} = result.data[0];
+            if(!this.updateTime || dayjs(updateTime).isAfter(dayjs(this.updateTime))){
+                this.emitEvent(ossUrl)
+                this.openQR=false
+                clearInterval(this.timer)
+            }
+           
+        },
+        handleDelete(){
+            this.emitEvent("")
+        },
+        qrCallback(e){
+            
+        },
+        emitEvent(data) {
+            this.$emit("input", data);
+        }
     }
 }
 </script>
 
 <style scoped lang="scss">
-.container-init {
-    background: linear-gradient(334deg, #2eb1e7, #0b137c 83%);
+.container {
     border: 1px solid #5c94e4;
-    border-image: linear-gradient(180deg, #3667fb 2%, #86efff) 2 2;
+
     border-radius: 10px;
-    filter: blur(10px);
+}
+.container-init {
+    // background: linear-gradient(334deg, #2eb1e7, #0b137c 83%);
+
+    
+    height: calc(100% - 20px);
+    margin: 10px;
     display: flex;
+    min-height: 80px;
+    box-sizing: border-box;
+    .left {
+        flex: 1;
+        margin-right:10px;
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .left-top {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 40px;
+            height: 40px;
+            border-top: 2px solid rgba(255, 255, 255, 0.6);
+            border-left: 2px solid rgba(255, 255, 255, 0.6);
+        }
+        .right-top {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 40px;
+            height: 40px;
+            border-top: 2px solid rgba(255, 255, 255, 0.6);
+            border-right: 2px solid rgba(255, 255, 255, 0.6);
+        }
+        .left-bottom {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 40px;
+            height: 40px;
+            border-bottom: 2px solid rgba(255, 255, 255, 0.6);
+            border-left: 2px solid rgba(255, 255, 255, 0.6);
+        }
+        .right-bottom {
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            width: 40px;
+            height: 40px;
+            border-bottom: 2px solid rgba(255, 255, 255, 0.6);
+            border-right: 2px solid rgba(255, 255, 255, 0.6);
+        }
+        .center {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items:center;
+            .content {
+                font-size: 20px;
+                color: #ffffff;
+                line-height: 25px;
+            }
+        }
+    }
+    .right {
+        width: 100px;
+        border-radius: 4px;
+        width: 100px;
+        background: linear-gradient(153deg,#ffd85a 1%, #f2901e 83%);
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        cursor:pointer;
+        .content{
+            margin-top:30px;
+            color: #ffffff;
+            line-height: 19px;
+            font-size: 15px;
+        }
+    }
+}
+.container-img{
+    height:100%;
+    overflow: hidden;
+    display:flex;
+    align-items: center;
+    position: relative;
+   .right-delete{
+       position: absolute;
+       right:0;
+       height:100%;
+       width:100px;
+       background:rgba(0,0,0,0.80);
+       display:flex;
+       flex-direction: column;
+       justify-content: center;
+       align-items: center;
+       color:#fff;
+       font-size:20px;
+       i{
+           font-size:30px;
+       }
+       
+   }
+}
+.dialog-body{
+    width:100%;
+    display:flex;
+    justify-content: center;
 }
 </style>
