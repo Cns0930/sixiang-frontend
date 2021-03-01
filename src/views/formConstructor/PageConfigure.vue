395b7adac62951499d898ed20e2f7fafc636d3fb
JiaxinWang <wangjiaxin-j@seassoon.com>
2021-03-01 17:21:41 +0800

    步骤页面配置config字段缺失bug修复

--- src/views/formConstructor/PageConfigure.vue
+++ src/views/formConstructor/PageConfigure.vue
@@ -373,7 +373,12 @@ export default {
         },
         tag_label() {
             return (fieldNo) => {
-                return this.fields.find(field => field.fieldNo === fieldNo).label;
+                if(this.fields.find(field => field.fieldNo === fieldNo)) {
+                    return this.fields.find(field => field.fieldNo === fieldNo).label;
+                } else {
+                    return '未找到自渎，请删除标签';
+                }
+                
             }
         }
 
