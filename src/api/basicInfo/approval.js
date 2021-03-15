bb57ce0baff3850b19e5617a5d28bb00fc2b5119
JiaxinWang <wangjiaxin-j@seassoon.com>
2021-03-15 16:28:44 +0800

    事项整份配置上传git接口修改

--- src/api/basicInfo/approval.js
+++ src/api/basicInfo/approval.js
@@ -43,4 +43,4 @@ export const shutApprovalItem = params => { return axios.get(`/ss/approvalItem/s
 export const listPublicApprovalItem = params => { return axios.get(`/ss/approvalItem/listPublicApprovalItem`, {params}).then(res=>res.data); };
 
 // 查询事项下所有帮办所需信息上传git
-export const submitItemInfo = params => { return axios.get(`/api/sixiang/submitItemInfo`, {params}).then(res=>res.data); };
+export const submitItemInfo = params => { return axios.get(`/ss/approvalItem/submitItemInfo`, {params}).then(res=>res.data); };
